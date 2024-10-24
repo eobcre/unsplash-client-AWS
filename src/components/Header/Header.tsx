dacedec430cbb2b7a45a8f364fc9cf222c6838d7
Emmy Manning <emswoo@EM.local>
2024-10-24 11:39:19 -0400

    added button component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import Nav from '../Header/Nav';
 import Search from '../Header/Search';
+import Button from '../Button';
 import Icon from '../../assets/icon.svg';
 
 const Header = () => {
@@ -11,7 +11,10 @@ const Header = () => {
           <img src={Icon} alt='Icon' width={50} />
           <Search />
         </div>
-        <Nav />
+        <div className='flex items-center gap-7'>
+          <Button buttonName='Login' buttonStyle='text-white bg-primary rounded-md px-8 py-2' />
+          <Nav />
+        </div>
       </div>
     </div>
   );
